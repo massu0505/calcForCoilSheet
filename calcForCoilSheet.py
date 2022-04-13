0abdead4a3184e1306135ee3217c5c2debb14c7e
massu0505 <masuda@hapis.k.u-tokyo.ac.jp>
2022-04-13 23:23:41 +0900

    fix error

--- calcForCoilSheet.py
+++ calcForCoilSheet.py
@@ -22,12 +22,12 @@ w=2*np.pi*f
 V1=50
 Q1=200
 Q2=700
-ke=np.linspace(0.05, 0.4, num=8)
-# ke=0.2
-# l_FromSourceToCp=np.linspace(0.0, 1.7, num=18)
-l_FromSourceToCp=1
+# ke=np.linspace(0.05, 0.4, num=8)
+ke=0.2
+l_FromSourceToCp=np.linspace(0, 1.0, num=21)
+# l_FromSourceToCp=1
 Sh_lambda=1.7
-Sh_Rsx=60
+Sh_Rsx=15
 Sh_C=1/(Sh_Rsx*Sh_lambda*f)
 Sh_L=Sh_Rsx/(Sh_lambda*f)
 Sh_beta=2*np.pi/Sh_lambda
@@ -48,9 +48,6 @@ Rl_opt=np.sqrt(r2**2+(r2*(w*Lm)**2)/(r1+Sh_Rsx))
 # Rl_opt=np.linspace(1, 10, num=10)
 # print("Rl_opt = " + str(Rl_opt))
 
-calcEtaAndZ_FromSource(w,l_FromSourceToCp,Sh_Rsx,Sh_beta,L1,Cs,r1,L2,C2,r2,Lm,Rl_opt)
-calcEtaAndZ_fromRL(w,Sh_Rsx,L1,r1,L2,r2,Lm)
-
 # %%
 def calcEtaAndZ_FromSource(w,l_FromSourceToCp,Sh_Rsx,Sh_beta,L1,Cs,r1,L2,C2,r2,Lm,RL):
 
@@ -62,8 +59,11 @@ def calcEtaAndZ_FromSource(w,l_FromSourceToCp,Sh_Rsx,Sh_beta,L1,Cs,r1,L2,C2,r2,L
     B=Z3-Z4
     Z5=Z4+B
     Zs=Z5-1j*(1/(w*Cs))
-    # print("Zs = " + str(Zs))
-    # print("B = " + str(B))
+    print("Z2 = " + str(Z2))
+    print("Z3 = " + str(Z3))
+    print("Z4 = " + str(Z4))
+    print("B = " + str(B))
+    print("Zs = " + str(Zs))
 
     Pr1=r1*(r2+RL)**2
     Pr2=r2*(w*Lm)**2
@@ -80,19 +80,26 @@ def calcEtaAndZ_FromSource(w,l_FromSourceToCp,Sh_Rsx,Sh_beta,L1,Cs,r1,L2,C2,r2,L
 def calcEtaAndZ_fromRL(w,Sh_Rsx,L1,r1,L2,r2,Lm):
 
     Z1=r1+2*Sh_Rsx+1j*w*L1
-    Z2=(w*Lm)**2/Z1
+    Z2=((w*Lm)**2)/Z1
     Z3=r1+1j*w*L2+Z2
     Xc=Z3.imag
     Ccoil=1/(w*Xc)
-    Z4=Z3-1/(w*Ccoil)
-    print("Ccoil = " + str(Ccoil))
+    # Z4=Z3-1/(w*Ccoil)
+    Z4=Z3.real
+    # print("Ccoil = " + str(Ccoil))
+    print("Z4_coil = " + str(Z4))
     
     Pr1=r1*((2*Sh_Rsx+r2)**2 + (w*Lm)**2)
     Pr2=r2*(w*Lm)**2
     Pz1=Sh_Rsx*(w*Lm)**2
     Pz2=Sh_Rsx*(w*Lm)**2
     eta=Pz1/(Pr1+Pr2+Pz1+Pz2)
-    print("eta_fromCoil = " + str(eta))
+    # print("eta_fromCoil = " + str(eta))
 
     return
+
+
+# %%
+calcEtaAndZ_FromSource(w,l_FromSourceToCp,Sh_Rsx,Sh_beta,L1,Cs,r1,L2,C2,r2,Lm,Rl_opt)
+calcEtaAndZ_fromRL(w,Sh_Rsx,L1,r1,L2,r2,Lm)
 # %%
