dccc3281d6b655093c6f85ad374d4eadd8854786
massu0505 <masuda@hapis.k.u-tokyo.ac.jp>
2022-04-13 13:37:19 +0900

    first commit

---
+++ calcForCoilSheet.py
@@ -0,0 +1,25 @@
+
+
+#%%
+from cmath import tan
+from http.client import NETWORK_AUTHENTICATION_REQUIRED
+import numpy as np
+import pandas as pd
+import re
+import matplotlib.pyplot as plt
+from plotly.graph_objs import Scatter3d
+# from plotly.offline import init_notebook_mode,iplot
+# init_notebook_mode()
+# from sklearn import linear_model
+# from scipy.optimize import curve_fit
+import os
+import csv
+import skrf as rf
+
+#%%
+
+
+
+
+
+
